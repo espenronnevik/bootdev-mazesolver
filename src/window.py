71be53b95b07e338e177823aab511a65188223c6
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Flesh out the Window class based on description

---
+++ src/window.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window(object):
+        def __init__(self, height, width):
+            self._root = Tk()
+            self._root.title("Maze Solver")
+            self._root.protocol("WM_DELETE_WINDOW", self.close)
+            self._canvas = Canvas(self._root, height=height, width=width)
+            self._canvas.pack()
+            self._running = False
+
+        def redraw(self):
+            self._root.update_idletasks()
+            self._root.update()
+
+        def wait_for_close(self):
+            self._running = True
+            while self._running:
+                self.redraw()
+
+        def close(self):
+            self._running = False
