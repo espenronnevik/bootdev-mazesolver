431f951a08c68860868b5ec440c56cfc53deb26e
Espen Rønnevik <espen@digitalsoup.eu>
2024-04-12 23:00:00 +0200

    Modify main() to use the new Maze class

--- src/main.py
+++ src/main.py
@@ -1,21 +1,19 @@
 from window import Window
-from geometry import Point, Line, Cell
+from maze import Maze
 
 def main():
-    win = Window(800, 800)
-    c1 = Cell(10, 10, 210, 210, win)
-    c1.has_right = False
-    c1.draw("black")
-    c2 = Cell(210, 10, 420, 210, win)
-    c2.has_left = False
-    c2.has_right = False
-    c2.draw("red")
-    c3 = Cell(410, 10, 630, 210, win)
-    c3.has_left = False
-    c3.draw("blue")
-    c1.draw_move(c2)
-    c2.draw_move(c3, undo=True)
+    offset_x = 5
+    offset_y = 5
+    num_rows = 10
+    num_cols = 10
+    cellsize_x = 100
+    cellsize_y = 100
 
+    winsize_x = (offset_x * 2) + (num_rows * cellsize_x)
+    winsize_y = (offset_y * 2) + (num_cols * cellsize_y)
+
+    win = Window(winsize_x, winsize_y)
+    maze = Maze(offset_x, offset_y, num_rows, num_cols, cellsize_x, cellsize_y, win)
     win.wait_for_close()
 
 if __name__ == "__main__":
