a56472609c8541d7fad7b67cf7d7420d701e9f58
Espen Rønnevik <espen@digitalsoup.eu>
2024-05-14 22:50:12 +0200

    Write a few tests for the Maze() logic

---
+++ src/test_maze.py
@@ -0,0 +1,26 @@
+import unittest
+
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_cell_count(self):
+        num_cols = 7
+        num_rows = 13
+        count = 0
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for i in range(num_cols):
+            count += len(m1._cells[i])
+        self.assertEqual(num_cols*num_rows, count)
