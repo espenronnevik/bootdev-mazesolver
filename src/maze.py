0a62e2d846ed30e3604d707666697360450baedc
Espen Rønnevik <espen@digitalsoup.eu>
2024-05-15 23:59:00 +0200

    Implement break_walls_r and helper function valid_cell

--- src/maze.py
+++ src/maze.py
@@ -18,6 +18,7 @@ class Maze(object):
 
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
         self._cells = []
@@ -50,3 +51,48 @@ class Maze(object):
         endcell = self._cells[-1][-1]
         endcell.has_right = False
         endcell.draw("black")
+
+    def _valid_cell(self, col, row):
+        if col < 0 or col >= self._num_cols or row < 0 or row >= self._num_rows:
+            return None
+        return self._cells[col][row]
+
+    def _break_walls_r(self, col, row):
+        currentcell = self._cells[col][row]
+        currentcell.visited = True
+
+        # Return early if we have broken into the "exit" cell
+        if currentcell == self._cells[-1][-1]:
+            self._draw_cells(currentcell)
+            return
+
+        valid_moves = {"up": [col - 1, row], "down": [col + 1, row], "right": [col, row + 1], "left": [col, row - 1]}
+        while True:
+            possible_moves = {}
+
+            for k, v in valid_moves.items():
+                cell = self._valid_cell(v[0], v[1])
+                if cell is not None and not cell.visited:
+                    possible_moves[k] = cell
+
+            if len(possible_moves) == 0:
+                self._draw_cells(currentcell)
+                return
+
+            rdirection = random.choice(list(possible_moves.keys()))
+            nextcell = possible_moves[rdirection]
+            if rdirection == "up":
+                currentcell.has_top = False
+                nextcell.has_bottom = False
+            elif rdirection == "right":
+                currentcell.has_right = False
+                nextcell.has_left = False
+            elif rdirection == "down":
+                currentcell.has_bottom = False
+                nextcell.has_top = False
+            elif rdirection == "left":
+                currentcell.has_left = False
+                nextcell.has_right = False
+
+            self._draw_cells(currentcell, nextcell)
+            self._break_walls_r(valid_moves[rdirection][0], valid_moves[rdirection][1])
